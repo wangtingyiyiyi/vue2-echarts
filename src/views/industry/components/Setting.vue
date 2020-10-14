7ec46059f13ae30deaa27341a3e2e452fdc05181
wangtingyi <1359666963@qq.com>
2020-10-14 11:14:20 +0800

    行业搜索高亮

--- src/views/industry/components/Setting.vue
+++ src/views/industry/components/Setting.vue
@@ -1,56 +1,68 @@
 <template>
-  <div>
-    <div class="industry-setting-wapper">
-      <el-form class="m-t-20" inline label-position="left" label-width="70px" ref="industryForm">
-        <el-form-item prop="brandId">
-          <span slot="label" class="form-label">选择行业</span>
-          <el-select
-            ref="select"
-            :value="categoryObj.label"
-            filterable
-            remote
-            placeholder="请输入搜索品类"
-            style="width: 600px"
-            @clear="handleClean"
-            :remote-method="handleSelctRemoteFilter">
-            <el-option value="0" class="hidden"></el-option>
-              <!-- 搜索树 -->
-              <el-tree
-                ref="tree"
-                v-show="showExpandTree"
-                :data="options"
-                node-key="key"
-                class="select-tree"
-                @node-click="handleNodeClick"
-                :default-expand-all="true"
-                :render-content="renderContent"
-                :props="defaultProps">
-              </el-tree>
-              <!-- 异步请求树 -->
-              <el-tree
-                v-show="!showExpandTree"
-                :props="leftTreeProps"
-                node-key="id"
-                :data="rootTree"
-                highlight-current
-                check-on-click-node
-                ref="leftTree1"
-                lazy
-                class="select-tree"
-                :render-content="renderContent"
-                @current-change="handleNodeClick"
-                :load="loadNode">
-              </el-tree>
-            </el-select>
-        </el-form-item>
-        <el-form-item>
-          <div style="display: flex">
-            <Text-Button text="高级筛选" @handleClick="handleFilter" class="p-0-15 font-size-14 m-l-5" />
-            <Text-Button text="行业提数" @handleClick="handleExportDialog" class="p-0-15 font-size-14" />
-          </div>
-        </el-form-item>
-      </el-form>
-    </div>
+  <div class="industry-setting-wapper">
+    <el-form class="m-t-20" inline label-position="left" label-width="70px" ref="industryForm">
+      <el-form-item prop="brandId">
+        <span slot="label" class="form-label">选择行业</span>
+        <el-select
+          ref="select"
+          :value="categoryObj.label"
+          filterable
+          remote
+          placeholder="请输入搜索品类"
+          style="width: 600px"
+          :remote-method="handleSelctRemoteFilter">
+          <el-option value="0" class="hidden"></el-option>
+            <!-- 搜索树 -->
+            <el-tree
+              ref="tree"
+              v-show="showExpandTree"
+              :data="options"
+              node-key="key"
+              class="select-tree"
+              @node-click="handleNodeClick"
+              :default-expand-all="true">
+              <span slot-scope="{node, data}">
+                <span class="custom-tree-node">
+                  <span class="tree-select-icon" v-if="data.remark === '1' || data.remark === 1" style="color: #5B8FF9;">{{data.remark}}</span>
+                  <span class="tree-select-icon" v-else-if="data.remark === '2' || data.remark === 2" style="color: #5AD8A6;">{{data.remark}}</span>
+                  <span class="tree-select-icon" v-else-if="data.remark === '3' || data.remark === 3" style="color: #5D7092;">{{data.remark}}</span>
+                  <span class="tree-select-icon" v-else style="color: #5B8FF9;">自</span>
+                  <span v-html="highlight(likeCondition, data.label)"></span>
+                </span>
+              </span>
+            </el-tree>
+            <!-- 异步请求树 -->
+            <el-tree
+              v-show="!showExpandTree"
+              :props="leftTreeProps"
+              node-key="id"
+              :data="rootTree"
+              highlight-current
+              check-on-click-node
+              ref="leftTree1"
+              lazy
+              class="select-tree"
+              @current-change="handleNodeClick"
+              :load="loadNode">
+              <span slot-scope="{node, data}">
+                <span class="custom-tree-node">
+                  <span class="tree-select-icon" v-if="data.remark === '1' || data.remark === 1" style="color: #5B8FF9;">{{data.remark}}</span>
+                  <span class="tree-select-icon" v-else-if="data.remark === '2' || data.remark === 2" style="color: #5AD8A6;">{{data.remark}}</span>
+                  <span class="tree-select-icon" v-else-if="data.remark === '3' || data.remark === 3" style="color: #5D7092;">{{data.remark}}</span>
+                  <span class="tree-select-icon" v-else style="color: #5B8FF9;">自</span>
+                  <span>{{data.label}}</span>
+                </span>
+              </span>
+            </el-tree>
+          </el-select>
+      </el-form-item>
+      <el-form-item>
+        <div style="display: flex">
+          <Text-Button text="高级筛选" @handleClick="handleFilter" class="p-0-15 font-size-14 m-l-5" />
+          <Text-Button text="行业提数" @handleClick="handleExportDialog" class="p-0-15 font-size-14" />
+        </div>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -69,17 +81,14 @@ export default {
         label: ''
       },
       options: [],
-      defaultProps: {
-        children: 'children',
-        label: 'label'
-      },
       showExpandTree: false,
       leftTreeProps: {
         isLeaf: 'isLeaf',
         children: 'childList',
         disabled: this.disabledFn
       },
-      rootTree: []
+      rootTree: [],
+      likeCondition: ''
     }
   },
   computed: {
@@ -87,15 +96,15 @@ export default {
   },
   methods: {
     ...mapMutations('industry', ['SET_INDUSTRY_CATEGORY']),
-    getEmptyText () {
-      const query = this.$refs?.select?.query || ''
-      return (query === '' && this.options.length === 0) ? '请输入关键字' : '暂无匹配数据'
-    },
     handleFilter () {
       this.$emit('handleFilter', true)
     },
     handleExportDialog () {
       this.$emit('handleExportDialog')
+    },
+    highlight (keyWord, label) {
+      const mes = label.replace(new RegExp(`(${keyWord})`, 'gi'), "<span style='background: #ffeb3b; font-weight: inherit'>$1</span>")
+      return '<span style="font-weight: inherit">' + mes + '</span>'
     },
     handleNodeClick (data, node, ref) {
       this.selectData.remark = data.remark
@@ -105,10 +114,8 @@ export default {
       this.$refs.select.blur()
       this.$emit('brandOnSubmit', this.selectData)
     },
-    handleClean () {
-      this.showExpandTree = false
-    },
     async handleSelctRemoteFilter (query) {
+      this.likeCondition = query
       if (query) {
         this.showExpandTree = true
         const res = await getIndustryCategory({ likeCondition: query })
@@ -126,52 +133,6 @@ export default {
       } else {
         this.showExpandTree = false
         this.options = []
-      }
-    },
-    handleChangeCurrent (data, node) {
-      console.info(data, node)
-    },
-    // render 函数渲染 tree 节点样式
-    renderContent (h, { node, data, store }) {
-      // "<span style='background: #ffeb3b; font-weight: inherit'>$1</span>"
-      // const likeCondition = 'cdbbdbsbz'
-      // const myRe = /d(b+)d/g
-      // console.info(myRe.exec(likeCondition))
-      // const rep = <span style='background: #ffeb3b; font-weight: inherit'>$1</span>
-      // const st = <span style="font-weight: inherit;background: #ffeb3b;">{rep}</span>
-      if (node.data.remark === '1' || node.data.remark === 1) {
-        return (
-          <span class="custom-tree-node">
-            <span class="tree-select-icon" style="color: #5B8FF9;">{node.data.remark}</span>
-            <span>{node.label}</span>
-          </span>)
-      } else if (node.data.remark === '2' || node.data.remark === 2) {
-        return (
-          <span class="custom-tree-node">
-            <span class="tree-select-icon" style="color: #5AD8A6;">{node.data.remark}</span>
-            <span>{node.label}</span>
-          </span>)
-      } else if (node.data.remark === '3' || node.data.remark === 3) {
-        return (
-          <span class="custom-tree-node">
-            <span class="tree-select-icon" style="color: #5D7092;">{node.data.remark}</span>
-            <span>{node.label}</span>
-          </span>)
-      } else {
-        return (
-          <span class="custom-tree-node">
-            <span class="tree-select-icon" style="color: #5B8FF9;">自</span>
-            <span>{node.label}</span>
-          </span>)
-      }
-    },
-    async getCat1 () {
-      const res = await getCategoryTree()
-      if (res.code === 200) {
-        this.options = res.result
-        this.options.forEach(item => {
-          item.label = item.outCat1
-        })
       }
     },
     async getCategoryTree (param = {}) {
@@ -202,9 +163,6 @@ export default {
         })
       }
     }
-  },
-  mounted () {
-    // this.getCat1()
   }
 }
 </script>
